1ed0585c38c4a4b8f86e6ac207de79d04410011d
Bartosz Prusak <bartosz.prusak@student.put.poznan.pl>
2023-04-25 20:19:12 +0200

    Add an option to disable upgraded cards #6

--- frontend/src/SingleDraft.js
+++ frontend/src/SingleDraft.js
@@ -1,5 +1,5 @@
 import React, {useEffect, useState} from "react";
-import {Container, Header} from "semantic-ui-react";
+import {Button, Checkbox, Container, Form, Header, Segment} from "semantic-ui-react";
 import ImpRandom from "./ImpRandom";
 import ConstructedCiv from "./ConstructedCiv.js";
 import CardStore, {MAIN_CATEGORIES} from "./cards/CardStore";
@@ -8,15 +8,18 @@ import CivCardGroup from "./cards/CivCardGroup";
 const SingleDraft = () => {
     const [collection, setCollection] = useState([]);
     const [selectedCards, setSelectedCards] = useState([]);
+    const [useUpgrades, setUseUpgrades] = useState(false);
+    const rerollDraft = () => {
+            let cards = [];
+            cards = [].concat(...(MAIN_CATEGORIES.map(cat => {
+                return CardStore.getUniqueCardsFromCategory(cat, 4, useUpgrades);
+            })));
+            cards.sort(ImpRandom.cardSort);
+            setCollection(cards);
+            setSelectedCards([]);
+    };
 
-    useEffect(() => {
-        let cards = [];
-        cards = [].concat(...(MAIN_CATEGORIES.map(cat => {
-            return CardStore.getUniqueCardsFromCategory(cat, 4);
-        })));
-        cards.sort(ImpRandom.cardSort);
-        setCollection(cards);
-    }, []);
+    useEffect(rerollDraft, []);
 
     const collectionCardClicked = (card) => {
         const maxOneOfCategory = MAIN_CATEGORIES.includes(card.cardCategory);
@@ -51,6 +54,16 @@ const SingleDraft = () => {
     return (
         <React.Fragment>
             <Container>
+                <Segment>
+                    <Header as='h3'>Single draft options</Header>
+                    <Form>
+                        <Form.Field>
+                            <Checkbox toggle label='Include upgraded (non-common) cards in the draft'
+                                checked={useUpgrades} onChange={(e, data) => setUseUpgrades(data.checked)}/>
+                        </Form.Field>
+                        <Button onClick={rerollDraft}>Draft new cards</Button>
+                    </Form>
+                </Segment>
                 <ConstructedCiv index='single draft' cards={selectedCards} editable={true} alwaysEditing={true}
                                 onCardClick={civCardClicked} />
             </Container>
--- frontend/src/cards/CardStore.js
+++ frontend/src/cards/CardStore.js
@@ -4,14 +4,10 @@ let allCards = [];
 
 export const CATEGORIES = ["CivilizationAbility", "LeaderAbility", "UniqueInfrastructure", "UniqueUnit", "Power", "ActOfGod"];
 export const MAIN_CATEGORIES = ["CivilizationAbility", "LeaderAbility", "UniqueInfrastructure", "UniqueUnit"];
+export const RARITY_WEIGHTS = {"Common": 15, "Uncommon": 4, "Rare": 3, "Epic": 2};
 
-const byCategory = {};
-const byTraitType = {};
+const byCategoryAndRarity = {};
 const mediaByCivType = {};
-
-CATEGORIES.forEach(cat => {
-    byCategory[cat] = [];
-})
 
 const CardStore = {
 
@@ -21,8 +17,13 @@ const CardStore = {
         if (!CardStore.initialised) {
             allCards = allCards.concat(cards);
             cards.forEach(card => {
-                byCategory[card.cardCategory].push(card);
-                byTraitType[card.traitType] = card;
+                if (!(card.cardCategory in byCategoryAndRarity)) {
+                    byCategoryAndRarity[card.cardCategory] = {};
+                }
+                if (!(card.rarity in byCategoryAndRarity[card.cardCategory])) {
+                    byCategoryAndRarity[card.cardCategory][card.rarity] = [];
+                }
+                byCategoryAndRarity[card.cardCategory][card.rarity].push(card);
                 if (card.cardCategory === 'CivilizationAbility') {
                     mediaByCivType[card.civilizationType] = card.mediaName;
                 }
@@ -39,16 +40,31 @@ const CardStore = {
         return mediaByCivType[civType];
     },
 
-    getCardByTraitType: (traitType) => {
-        return byTraitType[traitType];
-    },
-
-    getUniqueCardsFromCategory: (category, numCards, collection = []) => {
+    getUniqueCardsFromCategory: (category, numCards, upgraded=false) => {
         let selected = [];
+        let rarityWeightedList = ["Common"];
+
+        if (upgraded) {
+            rarityWeightedList = [].concat(...(
+                Object.keys(RARITY_WEIGHTS).map((rarity) => {
+                    let count = RARITY_WEIGHTS[rarity];
+                    return (rarity in byCategoryAndRarity[category]) ? Array(count).fill(rarity) : [];
+                })
+            ));
+        }
+        let failureCounter = 0;
         while (selected.length < numCards) {
-            const possibleCard = byCategory[category][ImpRandom.getRandomInt(byCategory[category].length)];
-            if (!selected.includes(possibleCard) && !collection.includes(possibleCard)) {
+            const rarity = rarityWeightedList[ImpRandom.getRandomInt(rarityWeightedList.length)];
+            const cardList = byCategoryAndRarity[category][rarity].filter(
+                card => !selected.some((c) => c.identifier == card.identifier));
+            if (cardList.length > 0) {
+                const possibleCard = cardList[ImpRandom.getRandomInt(cardList.length)];
                 selected.push(possibleCard);
+            } else {
+                failureCounter += 1;
+                if (failureCounter > 10) {
+                    break;
+                }
             }
         }
         return selected;
